09f638800ca95d1ee0fd61c9dc53124d2e069fbc
Ankur Dahal <48399122+dahalankur@users.noreply.github.com>
2021-01-27 11:48:11 +0545

    Refactored the class, added more public methods

--- tufts_dining_api/scraper.py
+++ tufts_dining_api/scraper.py
@@ -6,56 +6,96 @@ from tufts_dining_api.locations import locations
 
 # TODO: rename this class (and this file)
 class Dining():
-    def __init__(self):
-        self.location = None
-        self.locationId = None
-        self.date = None
+    def __init__(self, location):
+        self.location = location
+        if location in locations:
+            self._locationId = locations[self.location]
+        else:
+            self.location = None
+            self._locationId = None
 
 
     # TODO: support querying on the basis of date provided
     """
-    Returns the menu of the provided dining location
+    Returns the full menu of the dining location as a dictionary
     """
-    def getMenu(self, location, date=None):
-        if location in locations:
-            self.location = location
-            self.locationId = locations[self.location]
-
-            if date:
-                self.date = date # TODO: error checking here
+    def menu(self, date=None):
+        # check if valid location was provided in the constructor
+        if self.location:
+            if not date:
+                date = datetime.date.today()
             else: 
-                self.date = datetime.date.today() 
+                pass
+                # check the supplied date for errors
         
             # TODO: modify url to accommodate the date parameter
-            url = f"http://menus.tufts.edu/FoodPro%203.1.NET/shortmenu.aspx?sName=TUFTS+DINING&locationNum={self.locationId}&locationName={self.location}&naFlag=1"
-        
+            url = f"http://menus.tufts.edu/FoodPro%203.1.NET/shortmenu.aspx?sName=TUFTS+DINING&locationNum={self._locationId}&locationName={self.location}&naFlag=1"
             soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-            div_list = soup.findAll("div")
-    
-            response = dict()
-            menu_date = None  # TODO: modify this to use the supplied date instead
-
-            for div in div_list:
-                try:
-                    curr_div_class = div["class"][0]
-                    if not menu_date:
-                        if curr_div_class == "shortmenutitle":
-                            menu_date = "".join(div.text.split(",")[-2:]).strip()
-                            response["date"] = menu_date
-                    if curr_div_class == "shortmenumeals":
-                        menu_type = div.text.strip()
-                        if menu_type not in response:
-                            response[menu_type] = []
-                    elif curr_div_class == "shortmenucats":
-                        category = div.text.strip()
-                        category_dict = {category : []}
-                        response[menu_type].append(category_dict)
-                    elif curr_div_class == "shortmenurecipes":
-                        food_item = div.text.strip()
-                        category_dict[category].append(food_item)
-                    # TODO: account for duplicate categories and food_items
-                except:
-                    continue
-            return response
+            menu_date = None  # TODO: modify this to use the supplied date instead    
+            return self.__getResponse(soup)
         else:
-            return {"error" : "Invalid location"}
+            return {"Error" : "Invalid location provided"}
+           
+
+    def breakfast(self, date=None):
+        if self.location:
+            menu = self.menu()
+            if "Breakfast" in menu:
+                return menu["Breakfast"]
+            return {"Error" : "Breakfast not available for the selected location"}
+        else:
+            return {"Error" : "Invalid location provided"}
+    
+
+    def dinner(self, date=None):
+        if self.location:
+            menu = self.menu()
+            if "Dinner" in menu:
+                return menu["Dinner"]
+            return {"Error" : "Dinner not available for the selected location"}
+        else:
+            return {"Error" : "Invalid location provided"}
+    
+
+    def lunch(self, date=None):
+        if self.location:
+            menu = self.menu()
+            if "Lunch" in menu:
+                return menu["Lunch"]
+            return {"Error" : "Lunch not available for the selected location"}
+        else:
+            return {"Error" : "Invalid location provided"}
+
+    
+    """
+    Private helper method for constructing the response dict
+    """
+    def __getResponse(self, soup):
+        response = dict()
+        # menu_date = None
+        for div in soup.findAll("div"):
+            try:
+                curr_div_class = div["class"][0]
+                # if not menu_date:
+                #     if curr_div_class == "shortmenutitle":
+                #         menu_date = "".join(div.text.split(",")[-2:]).strip()
+                #         response["date"] = menu_date
+                if curr_div_class == "shortmenumeals":
+                    menu_type = div.text.strip()
+                    if menu_type not in response:
+                        response[menu_type] = []
+                elif curr_div_class == "shortmenucats":
+                    category = div.text.strip()
+                    category_dict = {category : []}
+                    response[menu_type].append(category_dict)
+                elif curr_div_class == "shortmenurecipes":
+                    food_item = div.text.strip()
+                    category_dict[category].append(food_item)
+                # TODO: account for duplicate categories and food_items
+            except:
+                continue
+        return response
+
+
+# carm = Dining("Carmichael Dining Center")
+# print(carm.menu()
